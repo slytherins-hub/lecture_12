4e00a2be67c0ba8ecee0a6e09c9d8c217b5c19b4
ejdamadam <247312@vut.cz>
2023-04-17 11:58:31 +0200

    added selection sort

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 def read_data(file_name):
@@ -8,8 +9,38 @@ def read_data(file_name):
     :param file_name: (str), name of CSV file
     :return: (dict), dictionary with numeric data, keys - csv column names, values - numbers in each column
     """
+
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, "r") as soubor:
+        data = csv.DictReader(soubor)
+        slovnik = {}
+        for row in data:
+            for hlavicka, hodnota in row.items():
+                if hlavicka not in slovnik:
+                    slovnik[hlavicka] = [int(hodnota)]
+                else:
+                    slovnik[hlavicka].append(int(hodnota))
+    return slovnik
+
+
+def selection_sort(num_list):
+    sorted_list = []
+    while num_list:
+        d = len(num_list)
+        x = num_list[0]
+        i = 1
+        while i < d:
+            if num_list[i] < x:
+                x = num_list[i]
+            i += 1
+        sorted_list.append(x)
+        num_list.remove(x)
+    return sorted_list
+
+
+print(selection_sort(read_data("numbers.csv")["series_1"]))
+
 
 
 def main():
