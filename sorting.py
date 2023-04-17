6bd790e666cca27391e6e3e5b3b2a87f808e4579
Martin Dvoulety <239452@vut.cz>
2023-04-17 10:26:38 +0200

    insertion_sort

--- sorting.py
+++ sorting.py
@@ -32,6 +32,15 @@ def selection_sort(values):
         values[i], values[min_idx] = values[min_idx], values[i]
     return values
 
+def bubble_sorting(values):
+    n = len(values)
+    for i in range(n-1):
+        for idx in range(n-i-1):
+            if values[idx] > values[idx+1]:
+                values[idx], values[idx+1] = values[idx+1], values[idx]
+            else:
+                pass
+    return values
 
 def main():
     pass
@@ -65,19 +74,47 @@ def bubble_sorting_2(values):
             pass
     return values
 
+def insertion_sort_2(values):
+    index = 0
+    while index < len(values)-1:
+        if values[index+1] < values[index]:
+            while values[index+1] < values[index] and index != -1:
+                vlozit = values.pop(index+1)
+                values.insert(index, vlozit)
+                index -= 1
+
+            index += 1
+        else:
+            index += 1
+    return values
+
+
 if __name__ == '__main__':
     my_data = read_data('numbers.csv')
-    print(my_data['series_2'])
     values = my_data['series_2'].copy()
     values_2 = my_data['series_1'].copy()
     values_3 = my_data['series_3'].copy()
+    values_4 = my_data['series_2'].copy()
+    values_5 = my_data['series_3'].copy()
+    print(my_data['series_2'])
+    print('selection_sort')
     print(selection_sort(values))
     print('\n')
+    print('selectionsort_2')
     print(values_2)
     print(selection_sort_2(values_2))
     print('\n')
+    print('bubble_sorting_2')
     print(values_3)
     print(bubble_sorting_2(values_3))
+    print('\n')
+    print('bubble_sorting')
+    print(values_4)
+    print(bubble_sorting(values_4))
+    print('\n')
+    print('insertin_sort_2')
+    print(values_5)
+    print(insertion_sort_2(values_5))
 
 
     '''
